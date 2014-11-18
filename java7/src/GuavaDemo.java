0f65c27bd3042eb6c8b0dab12a65a1c295d998f0
Anton Keks <anton@azib.net>
2014-11-19 01:30:27 +0200

    fluent iterable example

--- java7/src/GuavaDemo.java
+++ java7/src/GuavaDemo.java
@@ -1,6 +1,8 @@
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Ordering;
 
 import java.util.List;
 import java.util.regex.Pattern;
@@ -8,7 +10,9 @@ import java.util.regex.Pattern;
 import static com.google.common.base.Predicates.contains;
 import static com.google.common.base.Predicates.containsPattern;
 import static com.google.common.base.Predicates.or;
+import static com.google.common.collect.FluentIterable.from;
 import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Ordering.natural;
 import static java.lang.System.out;
 import static java.util.Arrays.asList;
 import static java.util.regex.Pattern.compile;
@@ -25,5 +29,11 @@ public class GuavaDemo {
         return s.startsWith("b");
       }
     }));
+
+    out.println(from(list).filter(new Predicate<String>() {
+      @Override public boolean apply(String s) {
+        return s.startsWith("a");
+      }
+    }).limit(5).toSortedList(natural().reverse()));
   }
 }
